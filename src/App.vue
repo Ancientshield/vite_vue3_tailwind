350cee72a5d03ecb20c4f1a2c243c418f066629b
Ancientshield <ancientsword520@gmail.com>
2022-12-28 15:50:25 +0800

    ProgressBar

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,26 @@
   <div class="flex justify-evenly p-10 focus-within:bg-green-100 group">
     <box v-for="item in Boxes" v-bind="item" :key="item.number" />
   </div>
+  <div :class="['w-screen h-screen',
+    'flex flex-col',
+    'justify-center items-center',
+    'gap-5',
+  ]">
+    <input type="range" v-model="val" :min="min" :max="max" />
+    <ProgressBar :currentVal="val" :minVal="min" :maxVal="max" />
+  </div>
 </template>
 
 <script>
 import Box from './components/Box.vue'
+import ProgressBar from './components/ProgressBar.vue'
 
 export default {
   data() {
     return {
+      min: 0,
+      max: 200,
+      val: 100,
       // 定義三個 box 的內容
       Boxes: [
         {number: 5, color: 'blue'},
@@ -19,6 +31,7 @@ export default {
     }
   },
   components: {
+    ProgressBar,
     Box
   }
 }
