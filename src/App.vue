2ae83efa3d748e9d416074d6b05af6abc6cc8b40
Ancientshield <ancientsword520@gmail.com>
2022-12-28 14:40:28 +0800

    vite + vue3 +tailwind

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld/>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
